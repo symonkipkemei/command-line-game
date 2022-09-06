ae6f5f0c60df45ec720edb22a5ac958ccc910ac5
symonkipkemei <symonkipkemei@gmail.com>
2022-09-06 15:13:10 +0300

    updates

--- main.py
+++ main.py
@@ -1,64 +1,147 @@
 
-from sub_programs import *
+from parameters import *
 
 
-def gameplay_left_door()-> bool:
-    """Activities that take place in the left door
+def gameplay_left_door(filepath, user_name = "player")-> bool:
+    """Game play : The left room is empty, but if you look around you will find a sword.
+
+    Args:
+        user_name (str): The name of the player
+
     Returns:
         bool: Return to the previous room (central pavillion), if not the game ends
     """
     return_previous_room = True
-    left_door()
-    user_option = option()
+    # what's in the room
+    what_is_in_the_room(f"Oops {user_name}! You entred an empty room")
+    user_option = option_to_interact()
     if user_option == 1:
-        further_option = option_further_left()
+        further_option = option_further("Look around")
+        item_name = "sword"
         if further_option == 1:
-            sword = sword_found()
+            sword_no = item_found(item_name)
+            record_inventory(filepath, item_name, sword_no)
         elif further_option == 2:
             pass
     return return_previous_room
 
-def gameplay_right_door(user_name:str)-> bool:
-    """Activities that take place in the right door
+def gameplay_right_door(filepath, user_name = "player")-> bool:
+    """There is a dragon in the right room, you have to fight it. To fight it you need at least one sword.If you win, there are items that you can collect.
 
     Args:
-        user_name (str): The name of the game user
+        user_name (str): The name of the player
 
     Returns:
         bool: Return to the previous room (central pavillion), if not the game ends
     """
-    right_door()
-    user_option = option()
+
+    print("\nFireeeee! I am the dragon, I am going to eat you alive !!")
+
+    user_option = option_to_interact()
+
     if user_option == 1:
-        further_option = option_further_right()
+        further_option = option_further("fight")
         if further_option == 1:
-            if sword == 1:
-                print(f"\nYou were born to win {user_name}, but to be a winner you must plan to win, prepare to win, and expect to win.\ncongratulations!")
-            
-            elif sword == 0:
-                print(f"\nSometimes by losing a battle,you find a new way to win the war.\nRest in peace {user_name}. ")
-            return_previous_room = False
+            #check if the user has a sword
+            record = retrieve_inventory(filepath)
+            for key, value in record.items():
+                if key == "sword" and int(value) > 0:
+                    print(f"\nYou were born to win {user_name}\ncongratulations the dragon is dead!")
+                    # interact_further
+                    option = option_further("Look beside the dragon", "Look behind the dragon")
+                    if option == 1:
+                        item = "mangoes"
+                         # There is a gun and a sword
+                        item_no = item_found(item)
+                        record_inventory(filepath, item,item_no)
+                    elif option == 2:
+                         # There is a gun and a sword
+                        item = "gun"
+                        item_no = item_found(item)
+                        record_inventory(filepath, item, item_no)
+                else:
+                    print("Rest in peace , we live to fight another day")
 
         elif further_option == 2:
             return_previous_room = True
 
-def main():
-    # before the start of the game ,the sword is 0
-    sword = 0
-    # introduction
-    name = player_name()
+def gameplay_back_door(filepath, user_name = "player")-> bool:
+    """Game play :There is a chimpanzee with a key, to get the key, you must feed it or kill it
 
-    # game begins
+    Args:
+        user_name (str): The name of the player
+
+    Returns:
+        bool: Return to the previous room (central pavillion), if not the game ends
+    """
+    print("\n Auu! Auu!, I am a chimpanzee, this is my territory!! , what do you want?")
+    
+    user_option = option()
+    if user_option == 1:
+        record = retrieve_inventory(filepath)
+        further_option = option_further("Fight the baboon", "Feed the baboon")
+        if further_option == 1:
+            # Gun needed to fight 
+            for key, value in record.items():
+                if key == "gun" and int(value) > 0:
+                    print(f"\nYou were born to win {user_name}\ncongratulations the the baboon is dead!")
+                    # interact_further
+                    option = option_further("Look beside the baboon")
+                    if option == 1:
+                       item_collected = "key"
+                       item_no = item_found(item_collected)
+                       record_inventory(filepath, item_collected, item_no)
+
+def gameplay_front_door(filepath, user_name = "player")-> bool:
+    """Game play: There is a princess , locked in this room. You need a key to rescue her.
+
+    Args:
+        user_name (_type_): The name of the player
+
+    Returns:
+        bool: Return to the previous room (central pavillion), if not the game ends
+    """
+
+    record = retrieve_inventory(filepath)
+
+    for key, value in record.items():
+        if key == "key" and int(value) > 0:
+            print(f"Congratulations {user_name}, you've just rescued the king's daughter!")
+            return False
+        else:
+            print(f"\n Ooops!,{user_name} The door appears to be closed, you need to find the key")
+
+    
+def main():
+    """The game is about finding a lost princess within the dungeons of the dragons. The princess is locked in one of the rooms! Will you be the hero! 
+    or will you be part of the statistics of the fallen soldiers!
+    """
+
+    # reset the inventory
+    filepath = "user_items.csv"
+    clean_inventory(filepath)
+
+    # introduction
+    name = "symon"
+
+    # Game begins, no way out unless you rescue the girl or you are defeated
     return_previous_room = True
     while return_previous_room:
         door_choice = door_choices()
         if door_choice == 1:
-            return_previous_room = gameplay_left_door()
+            return_previous_room = gameplay_left_door(filepath,name)
         elif door_choice == 2:
-            return_previous_room = gameplay_right_door(name)
+            return_previous_room = gameplay_right_door(filepath,name)
+        elif door_choice == 3:
+            return_previous_room = gameplay_front_door(filepath,name)
+        elif door_choice == 4:
+            return_previous_room = gameplay_back_door(filepath,name)
         else:
-            print("Try again")
+            print("Wrong input, try again")
 
     
 
+
 main()
+
+
--- parameters.py
+++ parameters.py
@@ -2,8 +2,16 @@
 """Parameters/Logic for the game,the thinniest ideas are organised in this directory"""
 
 import time
+import csv
 
-#Common parameters: applies to all rooms
+#Common parameters: applies to the game
+def what_is_in_the_room(message:str):
+    """Display what is in the room
+
+    Args:
+        message (str): Message to the player
+    """
+    print(message)
 
 def player_name() -> str:
     """Collect user_name and welcome him/her to the game
@@ -63,107 +71,190 @@ def player_name() -> str:
 
     return player
 
-
 def door_choices() -> int:
-    """Allows the player to make a choice between the available doors, The doors are : left, right, front, back
+    """Allows the player to make a choice between the available doors, The doors are : 
+    1. left
+    2. right
+    3. front
+    4. back
 
     Returns:
         int: the key pair of the chosen door
     """
-    print("\nThere are two doors\n(1).Left door\n(2).Right door\n(3.)Front door\n(4.)Back door")
-    door_choice = int(input("Make your choice: "))
+    print("\nThere are four doors\n*********************\n(1).Left door\n(2).Right door\n(3).Front door\n(4).Back door\n*********************")
+    door_choice = int(input("Make your choice:"))
+    print("\n")
+
 
     return door_choice
 
+# data storage,retrieving and cleaning
+def record_inventory(file_path, item: str,item_no: int):
+    """Records the collected item and the item number into a persistent memory(csv).
 
-def option() -> int:
+    Args:
+        item (str): The name of the collected item;
+        item_no(int): The number of items collected
+
+    Returns:
+        None: None
+    """
+    #retrieve information from stored media
+    inventory = {}
+    with open(file_path, "r") as f:
+        read_obj = csv.reader(f)
+        dd_list = list(read_obj)
+        
+        for x in dd_list:
+            inventory[x[0]] = x[1]
+        print(inventory)
+
+    # change the inventory based on the item added
+    for key,value in inventory.items():
+        if key == item:
+            inventory[key] = int(value) + item_no
+        
+    #save the changes back to the csv file
+    with open(file_path, "w") as f:
+        obj = csv.writer(f)
+        dd_list = []
+        for key, value in inventory.items():
+            data = [key, value]
+            dd_list.append(data)
+        
+        obj.writerows(dd_list)
+            
+def retrieve_inventory(file_path) -> dict:
+    """Opens a csv file, reads information and abstract data to a dictionary
+
+    Args:
+        file_path (csv): Opens a csv file
+
+    Returns:
+        dict: dictionary containing all the game inventory items
+    """
+    inventory = {}
+
+    with open(file_path, "r") as f:
+        obj_reader = csv.reader(f)
+        dd_list = obj_reader
+        for item in dd_list:
+            inventory[item[0]] = item[1]
+    
+    return inventory
+
+def clean_inventory(file_path):
+    """Clean the inventory before the game starts
+
+    Args:
+        file_path (csv): The path for the file storage
+    """
+    #reset items to 0
+    inventory = {"sword": 0, "key": 0, "gun":0, "mangoes": 0}
+
+    with open(file_path, "w") as f:
+        obj = csv.writer(f)
+        dd_list = []
+        for key, value in inventory.items():
+            data = [key, value]
+            dd_list.append(data)
+        
+        obj.writerows(dd_list)
+
+# options when playing the game, applies to all rooms
+
+def option_to_interact() -> int:
     """After entering the right,left,front or back door. The player has two options:
-     1. interacting further
-     2. returning to previous room
+     1. Interact further
+     2. Run to the previous room
 
     Returns:
         int: The key pair of the choice made by the player.
     """
     
-    print("\nYou have two options\n(1).Interact further\n(2).Run to the previous room")
-    user_option = int(input("Make your choice: "))
+    print("\nOptions:\n**************\n(1).Interact further\n(2).Run to the previous room\n**************")
+    option_user = int(input("Your choice: "))
 
-    return user_option
+    return option_user
 
+def item_found(item:str) -> int:
+    """After Looking around, the player spots an item, he/she has an option to take it or leave it. The item can be:
+    1. sword
+    2. key
+    3. gun
+    4. mangoes
 
-
-
-# Left room parameters
-def left_door():
-    """Game play at the left door: The room is empty
-    """
-    print("Oops! You've entered an empty room")
-
-def option_further_left() -> int:
-    """After selecting Interacting further the user has two options:
-    1. look around 
-    2. run to previous room
+    Args:
+        item (str): The name of the item
 
     Returns:
-        int: The key pair of the choice made by the player
+        int: The number of items picked by the player
     """
-    print("\nYou have two options\n(1).Look around\n(2). Run to the previous room")
-    user_option = int(input("Make your choice: "))
-    if user_option == 1:
-        return user_option
-    elif user_option == 2:
-        pass
-
-
-# Right room parameters
-
-def right_door():
-    """Gameplay at the right door: There is a dragon
-    """
-    print("\nFireeeee! I am the dragon, I am going to eat you alive !!")
-
-def option_further_right() -> int:
-    """After selecting interacting further, the player has two options in the right room,fight or escape the room
-
-    Returns:
-        int: The key pair of the choice made by the player
-    """
+    item_no = 0
+    try_again = True
+    while try_again:
+        print(f"\nYou have found a {str.capitalize(item)}!")
+        print("****************")
+        print("(1).Take it\n(2).Leave it")
+        print("****************")
+        user_option = int(input("Your choice: "))
+        if user_option == 1:
+            item_no += 1
+            print(f"\nYou now have a {item} in your collection! All the best in your search for the princess!")
+            try_again = False
+        elif user_option == 2:
+            item_no += 0
+            try_again = False
+        else:
+             print("Wrong input, try again")
+    return item_no
     
-    print("\nYou have two options\n(1).fight\n(2).Run to the previous room")
-    user_option = int(input("Make your choice: "))
-    return user_option
+def option_further(*args:str)-> int:
+    """Organise the arguments obtained into index, option and display to the player:
+    1. option 1: what happens?
+    2. option 2: what happens?
+    3. option 3: ..........(continues)
+    4. run to previous room (default)
 
-# Front room parameters
-
-def front_door():
-    """ Game play in front door : The door is locked"""  
-
-# Back room parameters
-def back_door():
-    """Game play in back door: There is a chimpanzee
-    """
-
-#if the user happens to find the sword, assign variable to one
-sword = 0
-
-def sword_found() -> int:
-    """After looking around in the left room, the player spots a sword. He has an option to take it or leave it.
-    When the player comes back to this room he can take another sword.
+    Args:
+        args (str): Options for the gameplay
 
     Returns:
-        int: The number of swords in possesion with the player,the default number is 0
+        int: The key pair of the choice made by the user.
+        None: when the player selects:run to the previous room.
     """
+    # organise the parameters obtained, organise into index,item and 
+    options_dict = {}
+    default_option = "run to the previous room"
+    for index, option in enumerate(args,1):
+        options_dict[index] = option
+
+    last_item = index + 1
+    options_dict[last_item] = default_option
+
+    try_again = True
+    while try_again:
+        # display to the user
+        print("\nOptions: ")
+        print("****************")
+        for key, value in options_dict.items():
+            print(f"({key}). {value}")
+        print("****************")
+        user_option = input("Your choice: ")
+
+        if user_option.isdigit():
+            user_option = int(user_option)
+            if user_option in options_dict.keys():
+                if user_option == last_item:
+                    try_again = False
+                    pass
     
-    global sword
-    print("\nYou have found a sword!\n(1).Take it\n(2).Leave it")
-    user_option = int(input("Make your choice: "))
-    if user_option == 1:
-        sword += 1
-        if sword == 1:
-            print("\nYou have sword now, may you win the battle.")
-        elif sword >= 2:
-            print("\nYou already have a sword in your hand, one sword is enough to win the battle.\nGood luck!")
-            sword = 1
-    elif user_option == 2:
-        sword = 0
-    return sword
+                else:
+                    try_again = False
+                    return user_option
+            else:
+                print("The option is not available, try again")
+        else:
+            print("Please insert a digit, try again: ")
+    
+
